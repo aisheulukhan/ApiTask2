e3b30087d197211363193d24d3e6bda8431be2d7
tu7hnsxs1 <tu7hnsxs1@code.edu.az>
2022-06-23 22:29:08 +0400

    add

---
+++ ApiTask/ApiTask1/ApiTask1/Data/Configiration/ActorController.cs
@@ -0,0 +1,23 @@
+﻿using ApiTask1.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTask1.Data.Configiration
+{
+    public class ActorController : IEntityTypeConfiguration<Actor>
+    {
+        public void Configure(EntityTypeBuilder<Actor> builder)
+        {
+            builder.Property(a => a.FullName).IsRequired().HasMaxLength(255);
+            builder.Property(a => a.ImageUrl).IsRequired();
+            builder.Property(a => a.IsDeleted).HasDefaultValue(false);
+            builder.Property(a => a.CreatedDate).HasDefaultValueSql("GETUTCDATE()");
+            builder.Property(a => a.ModifiedDate).HasDefaultValueSql("GETUTCDATE()");
+        }
+    }
+}
---
+++ ApiTask/ApiTask1/ApiTask1/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using ApiTask1.Dtos;
+using ApiTask1.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTask1.Mapping
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Actor, ActorCreateDto>().ReverseMap();
+            CreateMap<Actor, ActorCreateDto>().ReverseMap();
+        }
+    }
+}
